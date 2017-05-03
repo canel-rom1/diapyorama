5680cad17edc6b8ca96161500de75f7a63fbdfd3
Rom1 <rom1@canel.ch>
2017-05-03 20:50:17 +0200

    Début de la création de la fenêtre de configuration

--- slideshow.py
+++ slideshow.py
@@ -13,14 +13,33 @@ class Sql(object):
     """
     def __init__(self, dbfile):
         """Initialisation et connexion à la base de données"""
+
+        self.table = 'slides'
+
         conn_db = sqlite3.connect(dbfile)
         with conn_db:
             self.cursor = conn_db.cursor()
-            self.cursor.execute('SELECT * FROM slides')
 
-    def fetchData(self):
+    def sortedDelayLink(self):
         """
-           Retourner les données des temps et les liens des diapositives
+           Retourne les données des temps et les liens des diapositives. Ordre de la seq
+
+           Sortie:
+               < delays >: Délais de chaque diapositive
+               < links > : Lien pointant vers l'adresse de l'image
+        """
+        self.cursor.execute('SELECT req, delay, link FROM slides')
+        tab = self.cursor.fetchall()
+        delay = [0]*len(tab)
+        link = [0]*len(tab)
+        for s, d, l in tab:
+            delay[s-1] = d
+            link[s-1] = l
+        return delay, link
+
+    def fetchDelayLink(self):
+        """
+           Retourne les données des temps et les liens des diapositives. Ordre de l'id
 
            Sortie:
                < delays >: Délais de chaque diapositive
@@ -28,10 +47,15 @@ class Sql(object):
         """
         delays = []
         links = []
+        self.cursor.execute('SELECT delay, link FROM slides')
         for slide in self.cursor.fetchall():
-            delays.append(slide[1])
-            links.append(slide[2])
+            delays.append(slide[0])
+            links.append(slide[1])
         return (delays, links)
+
+    def fetchAll(self):
+        self.cursor.execute('SELECT * FROM slides')
+        return self.cursor.fetchall()
 
 
 class Slideshow(Frame):
@@ -50,6 +74,8 @@ class Slideshow(Frame):
         self.bind_all('<Escape>', self.keyQuit)
         self.bind_all('<space>', self.keyPlayPause)
         self.bind_all('<F11>', self.keyFullscreenToggle)
+        self.bind_all('<o>', self.keyOptionsMenu)
+        self.bind_all('<u>', self.keyUpdate)
 
         self.dbStart()
 
@@ -62,10 +88,20 @@ class Slideshow(Frame):
         self.__play_loop = False            # Initialisation de la variable contrôlant la boucle de lecture
         self.__i_slide = 0                  # Initialisation du pointeur de la diapositive
 
+        self.__win_opt_open = False          # Initialisation de la variable contrôlant l'activation de la fenêtre des options
+
         self.fullscreenOn()
         self.play()
 
+        #self.sortDelayLink()
+
         self.pack()
+
+
+    def sortDelayLink(self):
+        self.db = Sql(self.__dbfile)
+        print(self.db.fetchData())
+        #return delays, links
 
 
     #######
@@ -74,7 +110,7 @@ class Slideshow(Frame):
     def dbStart(self):
         """Connexion à la base de donnée"""
         self.db = Sql(self.__dbfile)
-        self.__delays, self.__links = self.db.fetchData()
+        self.__delays, self.__links = self.db.sortedDelayLink()
 
     def dbUpdate(self):
         """Mise à jour de la base de donnée"""
@@ -141,6 +177,17 @@ class Slideshow(Frame):
 
 
     #######
+    ### Options)
+
+    def optionsMenu(self):
+        """Instancie la fenêtre du menu d'options"""
+        self.optMenu = OptionWin(self)
+
+    def optionsMenuQuit(self):
+        """Ferme la fenêtre du menu d'options"""
+        self.optMenu.winQuit()
+
+    #######
     ### Keys actions
 
     def keyQuit(self,event):
@@ -151,7 +198,6 @@ class Slideshow(Frame):
         """Touche clavier pour inverser le mode pleine écran"""
         self.fullscreenToggle()
 
-
     def keyPlayPause(self, event):
         """Touche Play/Pause"""
         if self.__play_loop:
@@ -159,8 +205,54 @@ class Slideshow(Frame):
         else:
             self.play()
 
+    def keyOptionsMenu(self, event):
+        """Touche pour activer la fenêtre d'option"""
+        if self.__win_opt_open:
+            self.__win_opt_open = False
+            self.optionsMenuQuit()
+        else:
+            self.__win_opt_open = True
+            self.optionsMenu()
+
+    def keyUpdate(self, event):
+        self.dbUpdate()
+
+
+class OptionWin(Toplevel):
+    def __init__(self, master = None):
+        Toplevel.__init__(self, master)
+        optSql = OptionsSql(self)
+        optSql.pack()
+
+    def winQuit(self):
+        self.destroy()
+
+
+class OptionsSql(Frame):
+    def __init__(self, master = None):
+        Frame.__init__(self, master)
+        global dbfile
+        fichier = dbfile
+        self.sql = Sql(fichier)
+
+        Label(self, text = 'Options Slite3 {}'.format(fichier)).pack()
+
+        self.table()
+
+    def table(self):
+        tableau = self.sql.fetchAll()
+        line = []
+        for i in range(len(tableau)):
+            varline[i] = StringVar()
+            varline[i].set(tableau[i])
+            line[i] = Entry(self, textvariable = varline[i])
+
+
 
 if __name__ == '__main__':
     dbfile = './slides/slides.db'
 
     Slideshow(dbfile = dbfile).mainloop()
+
+
+# vim: ft=python ts=8 et sw=4 sts=4
