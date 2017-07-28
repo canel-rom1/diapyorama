e8ea6561254837956001f8022a82081b30d7f8b1
Rom1 <rom1@canel.ch>
2017-07-28 12:54:31 +0200

    add: check_update.sh

---
+++ tools/check_update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+git remote update
+
+local=`git rev-list max-count=1 stable`
+distant=`git rev-list max-count=1 origin/stable`
+
+if [ "$local" != "$distant" ]
+then
+	kill `ps -eF | grep slideshow.py | head -n1 | awk '{ print $2 }'`
+	git pull --rebase origin stable
+	python3 /home/diapy/bin/slideshow.py
+fi
+
