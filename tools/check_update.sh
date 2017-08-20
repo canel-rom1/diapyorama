c3f326808be188eaba7394c1d49b055239e59844
Rom1 <rom1@canel.ch>
2017-08-20 21:47:34 +0200

    change name stable -> latest

--- tools/check_update.sh
+++ tools/check_update.sh
@@ -3,13 +3,13 @@
 
 git remote update
 
-local=`git rev-list --max-count=1 stable`
-distant=`git rev-list --max-count=1 origin/stable`
+local=`git rev-list --max-count=1 latest`
+distant=`git rev-list --max-count=1 origin/latest`
 
 if [ "$local" != "$distant" ]
 then
 	kill `ps -eF | grep slideshow.py | head -n1 | awk '{ print $2 }'`
-	git pull --rebase origin stable
+	git pull --rebase origin latest
 	python3 /home/diapy/bin/slideshow.py
 fi
 
